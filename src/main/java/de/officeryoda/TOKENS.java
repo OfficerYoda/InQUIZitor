a156532fc258602795d75e1c17571163037a6b0f
OfficerYoda <89543854+OfficerYoda@users.noreply.github.com>
2022-10-19 17:23:07 +0200

    Delete TOKENS.java

--- src/main/java/de/officeryoda/TOKENS.java
+++
@@ -1,8 +0,0 @@
-package de.officeryoda;
-
-public class TOKENS {
-
-    public static final String TOKEN = "MTAyOTAyMTc3NzI3NzUwNTU5OA.G6a-8x.Yz1BjLPZJkv3UfwU3Jf_W4BgqbuK-kCVLqWOzA";
-    public static final String PREFIX = "?";
-
-}
